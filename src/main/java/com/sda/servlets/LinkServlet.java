0c9f6f0d2fe6403c6b7a4ebe1bc6ae55284aae7f
M <szumagda@wp.pl>
2018-11-14 18:27:16 +0100

    links | refactor to new class

--- src/main/java/com/sda/servlets/LinkServlet.java
+++ src/main/java/com/sda/servlets/LinkServlet.java
@@ -6,19 +6,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LinkServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
 
-        resp.addHeader("Content-Type", "text/html");
+        List <Link> links = new ArrayList<>();
+        links.add(new Link("https://www.google.pl","Google"));
+        links.add(new Link("https://www.onet.pl","Onet"));
+        links.add(new Link("https://www.wp.pl","Wp"));
 
-        writer.println("<br/>\n" +
-                "\t<a href =\"https://www.google.pl\">Google</a>\n" +
-                "<br/>\n" +
-                "\t<a href =\"https://www.onet.pl/\">Onet</a>\t\n" +
-                "<br/>\n" +
-                "\t<a href =\"https://www.wp.pl/\">Wp</a>\t"    );
+        resp.addHeader("Content-Type", "text/html");
+        writer.println("<div>");
+        writer.println("Moje linki:");
+        for (Link link: links) {
+            writer.println("<br/>");
+            writer.println("<a href=\"" + link.getUrl() + "\">" + link.getText() + "</a>");
+        }
+        writer.println("</div>");
+/*
+        writer.println("<br/>");
+        writer.println("<a href=\"http://www.google.com\">Google</a>");
+        writer.println("<br/>");
+        writer.println("<a href=\"http://pl.wikipedia.com\">Wikipedia</a>");
+        writer.println("<br/>");
+        writer.println("<a href=\"http://onet.pl\">Onet</a>");
+
+        */
     }
 }
